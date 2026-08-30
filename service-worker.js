5d88d140b2d9f6d5016d228303ad906fff527337
melinahargrove-droid <melina.hargrove@gmail.com>
2026-08-30 10:56:13 -0500

    Update service worker cache to build 37

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="early-eagle-label-maker-v36";
-const APP_SHELL=["./","./index.html?appv=36","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./make-list-hotfix.js?v=36","./pwa-version-fix.js?v=36"];
+const CACHE="early-eagle-label-maker-v37";
+const APP_SHELL=["./","./index.html?appv=37","./manifest.webmanifest","./icon-192.png","./icon-512.png","./icon-maskable-512.png","./make-list-hotfix.js?v=37","./pwa-version-fix.js?v=37"];
 
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP_SHELL)));
@@ -15,8 +15,8 @@ async function injectHotfix(response){
   if(!response) return response;
   try{
     const text=await response.text();
-    const listTag='<script src="./make-list-hotfix.js?v=36"></script>';
-    const versionTag='<script src="./pwa-version-fix.js?v=36"></script>';
+    const listTag='<script src="./make-list-hotfix.js?v=37"></script>';
+    const versionTag='<script src="./pwa-version-fix.js?v=37"></script>';
     let html=text;
     if(!html.includes("make-list-hotfix.js")) html=html.replace("</body>",listTag+"</body>");
     if(!html.includes("pwa-version-fix.js")) html=html.replace("</body>",versionTag+"</body>");
@@ -36,7 +36,7 @@ self.addEventListener("fetch",e=>{
       try{
         return await injectHotfix(await fetch(e.request,{cache:"no-store"}));
       }catch{
-        return await injectHotfix(await caches.match("./index.html?appv=36"));
+        return await injectHotfix(await caches.match("./index.html?appv=37"));
       }
     })());
     return;
